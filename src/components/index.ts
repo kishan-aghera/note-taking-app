689f3c4ebfae5094bb019dfcdd1a2353ead70e92
kishan-aghera <55836816+kishan-aghera@users.noreply.github.com>
2023-10-12 14:24:42 +0530

    feat: Edit/Delete tags.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,17 @@
 import EditNote from "./EditNote";
+import EditTagsModal from "./EditTagsModal";
 import NewNote from "./NewNote";
 import Note from "./Note";
 import NoteForm from "./NoteForm";
 import NoteLayout from "./NoteLayout";
 import NoteList from "./NoteList";
 
-export { NewNote, NoteForm, NoteList, NoteLayout, Note, EditNote };
+export {
+  NewNote,
+  NoteForm,
+  NoteList,
+  NoteLayout,
+  Note,
+  EditNote,
+  EditTagsModal,
+};
